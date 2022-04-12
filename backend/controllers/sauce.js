bb140edefe9a31e1b37b9f0e741c7559961faf1f
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-04-12 13:10:03 +0200

    Création des routes et des controllers pour les sauces

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,7 @@
+const Sauce = require("../models/Sauces");
+
+exports.getAllSauce = (req, res, next) => {};
+exports.getOneSauce = (req, res, next) => {};
+exports.createSauce = (req, res, next) => {};
+exports.modifySauce = (req, res, next) => {};
+exports.deleteSauce = (req, res, next) => {};
--- backend/router/sauce.js
+++ backend/router/sauce.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const sauceCtrl = require("../controllers/sauce");
 
-router.get("/" + "", sauceCtrl);
+router.get("/" + "", sauceCtrl.getAllSauce);
+router.get("/:id", sauceCtrl.getOneSauce);
+router.post("/", sauceCtrl.createSauce);
+router.put("/:id", sauceCtrl.modifySauce);
+router.delete("/:id", sauceCtrl.deleteSauce);
 
 module.exports = router;
