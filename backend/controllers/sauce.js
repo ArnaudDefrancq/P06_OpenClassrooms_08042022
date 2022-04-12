324e26056bdd69a1c9bf4d6c5fdb2bf188a330fa
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-04-12 14:21:22 +0200

    Affichage de toutes les sauces disponible dans la base de donnée

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,7 +1,17 @@
 const Sauce = require("../models/Sauces");
 
-exports.getAllSauce = (req, res, next) => {};
+exports.getAllSauce = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => {
+      res.status(200).json(sauces);
+    })
+    .catch((error) => res.status(400).json({ error: error }));
+};
+
 exports.getOneSauce = (req, res, next) => {};
+
 exports.createSauce = (req, res, next) => {};
+
 exports.modifySauce = (req, res, next) => {};
+
 exports.deleteSauce = (req, res, next) => {};
